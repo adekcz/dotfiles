9ca60efe27804f20ecb67764364851ce178bd5cc
adekcz <adekcz@gmail.com>
2017-03-11 15:09:22 +0100

    Vim plugin installation semiautomation script

---
+++ installedVimPlugins.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/tpope/vim-pathogen.git autoload
+git clone https://github.com/ctrlpvim/ctrlp.vim.git bundle
+git clone https://github.com/vim-syntastic/syntastic.git bundle
+git clone https://github.com/vim-airline/vim-airline.git bundle
+git clone https://github.com/vim-airline/vim-airline-themes.git bundle
+git clone https://github.com/easymotion/vim-easymotion.git bundle
